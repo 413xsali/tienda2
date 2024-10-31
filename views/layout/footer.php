78a9990f3c35e3347ae1e50aebc8d9de95b25952
413xsali <alexsanchezli.2005@gmail.com>
2024-10-31 15:58:50 -0500

    3:58

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,9 +1,9 @@
 </div>
-</div>
-<footer id="pie de pagina">
-    <p>Desarrollado por Alex Sanchez Linares web &copy; 2024</p>
-</footer>
-</div>
+        </div>
+        
+        <footer id="footer">
+            <p>Desarrollado por Alex sanchez Web &copy; 2024</p>
+        </footer>
+    </div>
 </body>
-
 </html>
