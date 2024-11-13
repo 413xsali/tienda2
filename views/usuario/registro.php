61266222ccd641ee8d57b145085cce0503b5fd0f
413xsali <alexsanchezli.2005@gmail.com>
2024-11-13 15:16:07 -0500

    3:16

---
+++ views/usuario/registro.php
@@ -0,0 +1,15 @@
+<form action="" method="POST">
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nombre" required/>
+
+    <label for="apellidos">Apellidos</label>
+    <input type="text" name="apellidos" required/>
+
+    <label for="email">Email</label>
+    <input type="email" name="email" required/>
+
+    <label for="password">Contraseña</label>
+    <input type="text" name="password" required/>
+
+    <input type="submit" value="Registrarse" />
+</form>
